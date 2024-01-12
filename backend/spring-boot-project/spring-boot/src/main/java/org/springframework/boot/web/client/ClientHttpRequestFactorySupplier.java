a56d2cf60c2613b4bd5792a38108d04d409933d1
Anton Samarin <Anton.Samarin@jetbrains.com>
2024-01-12 15:44:41 +0100

    First commit

---
+++ backend/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/client/ClientHttpRequestFactorySupplier.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.web.client;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.TypeHint.Builder;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.util.ClassUtils;
+
+/**
+ * A supplier for {@link ClientHttpRequestFactory} that detects the preferred candidate
+ * based on the available implementations on the classpath.
+ *
+ * @author Stephane Nicoll
+ * @author Moritz Halbritter
+ * @since 2.1.0
+ */
+public class ClientHttpRequestFactorySupplier implements Supplier<ClientHttpRequestFactory> {
+
+	private static final boolean APACHE_HTTP_CLIENT_PRESENT = ClassUtils.isPresent("org.apache.http.client.HttpClient",
+			null);
+
+	private static final boolean OKHTTP_CLIENT_PRESENT = ClassUtils.isPresent("okhttp3.OkHttpClient", null);
+
+	@Override
+	public ClientHttpRequestFactory get() {
+		if (APACHE_HTTP_CLIENT_PRESENT) {
+			return new HttpComponentsClientHttpRequestFactory();
+		}
+		if (OKHTTP_CLIENT_PRESENT) {
+			return new OkHttp3ClientHttpRequestFactory();
+		}
+		return new SimpleClientHttpRequestFactory();
+	}
+
+	static class ClientHttpRequestFactorySupplierRuntimeHints {
+
+		static void registerHints(RuntimeHints hints, ClassLoader classLoader, Consumer<Builder> callback) {
+			hints.reflection().registerType(HttpComponentsClientHttpRequestFactory.class, (hint) -> callback
+					.accept(hint.onReachableType(TypeReference.of("org.apache.http.client.HttpClient"))));
+			hints.reflection().registerType(OkHttp3ClientHttpRequestFactory.class,
+					(hint) -> callback.accept(hint.onReachableType(TypeReference.of("okhttp3.OkHttpClient"))));
+			hints.reflection().registerType(SimpleClientHttpRequestFactory.class, (hint) -> callback
+					.accept(hint.onReachableType(TypeReference.of(SimpleClientHttpRequestFactory.class))));
+		}
+
+	}
+
+}
